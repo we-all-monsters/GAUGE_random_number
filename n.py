da474c29abe07cb2e6db2ad0da4eff7873d120e9
weallmonsters <weallmonsters@gmail.com>
2019-04-07 22:19:35 +0800

    加次數

--- n.py
+++ n.py
@@ -1,15 +1,16 @@
 import random
 r = random.randint(1, 100)
-
+time = 0
 while True:
+	time += 1 #time = time +1
 	n = input('你估的數字：')
 	n = int(n)
 	if n > r:
 		print('估細d')
 	elif n < r:
 		print('估大d')
-	else :
+	elif n == r :
 		print('你對啦~')
+		print ('你用了' , time , '次，就估中了')
 		break
-	
-	
+	print ('你用了' , time , '次')
